4bea8982ac4d6cff7d2a2de87f2f82980313e75c
eshackleton13@apu.edu <eshackleton13@apu.edu>
2019-02-01 02:17:36 -0800

    auto commit

--- src/App.js
+++ src/App.js
@@ -1,15 +1,19 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import inner_pi from './components/inner_pi.png';
-import blankclock from './components/blankclock.png';
+import React, { Component} from 'react';
+import inner_pi from './components/images/inner_pi.png';
+import { Button } from 'react-bootstrap/Button';
+import { ButtonToolbar } from 'react-bootstrap/ButtonToolbar';
+import { Container } from 'react-bootstrap/Container';
 import './App.css';
 
-class App extends Component {
+class App extends React.Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <h3>This is the code portfolio of Elyse Shackleton</h3>
+          <h3><code>
+            Elyse Shackleton Code Portfolio
+            </code></h3>
+          <hr width="75%"/>
           <p>Follow @ <a 
             className="App-link"
             href="https://twitter.com/elyseshackleton"
@@ -25,10 +29,18 @@ class App extends Component {
           Instagram
           </a>
           </p>
+          <div>
           <img src={inner_pi} className="App-logo" alt="logo"/>
+          </div>
           <p>
             <highlight>Fight me irl</highlight>
           </p>
+          {/* <Container>
+            <ButtonToolbar>
+              <Button varient="primary">No Fight</Button>
+              <Button varient="warning">Yoinks</Button>
+            </ButtonToolbar> 
+          </Container> */}
           <a
             className="App-link"
             href="https://elyshack.github.io/testpage"
---
+++ src/components/Heading.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+
+
+class Heading extends Component {
+    render() {
+      return (
+          <p text-align="left">elyshack.github.io</p>
+      );
+    }
+} export default Heading;
