51427f2873510782d820e3004a8afeb342c756a1
eshackleton13@apu.edu <eshackleton13@apu.edu>
2019-08-21 16:31:59 -0700

    auto commit

--- src/App.js
+++ src/App.js
@@ -11,6 +11,7 @@ import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 import RecipeScaler from './screens/RecipeScaler.js';
 import Home from './screens/Home.js';
 import firebase from './firebase.js';
+import ApuTools from './screens/aputools.js';
 
 class App extends React.Component {
 
@@ -34,8 +35,9 @@ class App extends React.Component {
                       <Dropdown.Item eventKey="1" href="https://elyshack.github.io/testpage"
                           target="_blank"
                           rel="noopener noreferrer">Html test page</Dropdown.Item>
-                         <Dropdown.Item eventKey="2"> <Link className="sublink" to="/portfolio/recipe">Recipe Scaler</Link> </Dropdown.Item>
-                         <Dropdown.Item eventKey="3">Album</Dropdown.Item>
+                         <Dropdown.Item eventKey="2"> <Link className="sublink" to="/portfolio/aputools">APU IoT Tools</Link> </Dropdown.Item>
+                         <Dropdown.Item eventKey="3"> <Link className="sublink" to="/portfolio/recipe">Recipe Scaler[WIP]</Link> </Dropdown.Item>
+                         <Dropdown.Item eventKey="4">Album</Dropdown.Item>
                   </DropdownButton>
 
               <Button href={agent_file} variant="outline-primary" download="Elyse_Shackleton_Resume_June_2018.pdf">Resume</Button>
@@ -49,6 +51,8 @@ class App extends React.Component {
 
           <Route exact path="/portfolio/" component={Home} />
           <Route path="/portfolio/recipe" component={RecipeScaler} />
+          <Route path="/portfolio/aputools" component={ApuTools} />
+
 
         </Router>
 
---
+++ src/screens/aputools.js
@@ -0,0 +1,62 @@
+import inner_pi from '../components/images/inner_pi.png';
+import React, { Component } from 'react';
+import '../App.css';
+import Button from 'react-bootstrap/Button';
+import firebase from '../firebase.js';
+
+class ApuTools extends React.Component {
+
+  state = {number: 0}
+
+  testClick = () => {
+    this.setState({number: Math.floor(Math.random() * Math.floor(101))});
+    console.log(this.state.number);
+  }
+
+
+
+    // RANDOM STUDENT VALUE GENERATOR
+    handleClick = async () => {
+
+      var keyArray = [];
+      var dataArray =[];
+      try{
+        // Create Query
+        var studentsQuery = firebase.database().ref('students').orderByKey();
+        // Call query and loop through all fetched values
+         await studentsQuery.once("value").then(function(snapshot){
+            snapshot.forEach(function(childSnapshot){
+                var key = childSnapshot.key;
+                var childData = childSnapshot.val();
+                keyArray.push(key);
+                dataArray.push(childData);
+            });
+        });
+      } catch(err){
+        this.setState({number: err});
+          console.log(err);
+      }
+
+      // Build out object of random numbers corresponding with keys, then update
+      var obj = {};
+      for (var i=0; i < dataArray.length; i++){
+        var randomVal = Math.floor(Math.random() * Math.floor(101));
+        obj[keyArray[i]] = randomVal;
+      }
+
+      await firebase.database().ref('students/').update(obj);
+    }
+
+
+    render() {
+      return (
+      <div>
+      <div/>
+      <Button onClick={this.handleClick}>Randomize Database!</Button>
+      <div/>
+      </div>
+      
+      );
+    }
+  }
+  export default ApuTools;
