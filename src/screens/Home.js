3e0f70b8a639d715bf0bea98bcbfcf77dd04d838
eshackleton13@apu.edu <eshackleton13@apu.edu>
2019-08-21 16:33:55 -0700

    auto commit

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -6,51 +6,6 @@ import firebase from '../firebase.js';
 
 class Home extends React.Component {
 
-  state = {number: 0}
-
-  testClick = () => {
-    this.setState({number: Math.floor(Math.random() * Math.floor(101))});
-    console.log("HELLO");
-  }
-
-
-
-    // RANDOM STUDENT VALUE GENERATOR
-    handleClick = async () => {
-      this.setState({number: "yeet"});
-
-      var keyArray = [];
-      var dataArray =[];
-      try{
-        // Create Query
-        var studentsQuery = firebase.database().ref('students').orderByKey();
-        // Call query and loop through all fetched values
-         await studentsQuery.once("value").then(function(snapshot){
-            snapshot.forEach(function(childSnapshot){
-                var key = childSnapshot.key;
-                var childData = childSnapshot.val();
-                keyArray.push(key);
-                dataArray.push(childData);
-            });
-        });
-      } catch(err){
-        this.setState({number: err});
-          console.log(err);
-      }
-
-      // Build out object of random numbers corresponding with keys, then update
-      var obj = {};
-      for (var i=0; i < dataArray.length; i++){
-        var randomVal = Math.floor(Math.random() * Math.floor(101));
-        obj[keyArray[i]] = randomVal;
-      }
-
-      await firebase.database().ref('students/').update(obj);
-
-    }
-
-
-
 
     render() {
       return (
@@ -74,21 +29,6 @@ class Home extends React.Component {
       <img flex="1"
       src={inner_pi} className="App-logo" alt="logo"/>
       </div>
-      <p>
-        <highlight>Fight me irl</highlight>
-      </p>
-      <a
-        className="App-link"
-        href="https://elyshack.github.io/testpage"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        Test Page Link
-      </a>
-      <div/>
-      <Button onClick={this.handleClick}>Randomize!</Button>
-      <div/>
-      <label>{this.state.number}</label>
       </div>
       
       );
