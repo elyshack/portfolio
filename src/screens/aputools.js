94aeecc00d3cdf35687d0b9abacc81278f9331e0
eshackleton13@apu.edu <eshackleton13@apu.edu>
2019-08-21 17:10:13 -0700

    auto commit

--- src/screens/aputools.js
+++ src/screens/aputools.js
@@ -17,7 +17,6 @@ class ApuTools extends React.Component {
 
     // RANDOM STUDENT VALUE GENERATOR
     handleClick = async () => {
-
       var keyArray = [];
       var dataArray =[];
       try{
@@ -47,12 +46,43 @@ class ApuTools extends React.Component {
       await firebase.database().ref('students/').update(obj);
     }
 
+    funClick = async () => {
+        var keyArray = [];
+        var dataArray =[];
+        try{
+          // Create Query
+          var studentsQuery = firebase.database().ref('students').orderByKey();
+          // Call query and loop through all fetched values
+           await studentsQuery.once("value").then(function(snapshot){
+              snapshot.forEach(function(childSnapshot){
+                  var key = childSnapshot.key;
+                  var childData = childSnapshot.val();
+                  keyArray.push(key);
+                  dataArray.push(childData);
+              });
+          });
+        } catch(err){
+          this.setState({number: err});
+            console.log(err);
+        }
+  
+        // Build out object of random numbers corresponding with keys, then update
+        var obj = {};
+        for (var i=0; i < dataArray.length; i++){
+          var randomVal = Math.floor(Math.random() * Math.floor(101));
+          obj[keyArray[i]] = randomVal;
+          await firebase.database().ref('students/').update(obj);
+        }
+
+      }
+
 
     render() {
       return (
       <div>
       <div/>
-      <Button onClick={this.handleClick}>Randomize Database!</Button>
+      <Button onClick={this.handleClick}>Efficient Randomize Database</Button>
+      <Button onClick={this.funClick}>FUN Database Randomize!</Button>
       <div/>
       </div>
       
